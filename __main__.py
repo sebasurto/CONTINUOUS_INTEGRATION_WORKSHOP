318f1982aef4a84e440e4ae598aefa5e83f5923b
sergio basurto cedeño <sbasurto@espol.edu.ec>
2023-12-06 22:57:47 -0500

    Creating Library

---
+++ __main__.py
@@ -0,0 +1,107 @@
+import csv
+from datetime import datetime, timedelta
+
+class Book:
+    def __init__(self, title, author, available_quantity):
+        self.title = title
+        self.author = author
+        self.available_quantity = available_quantity
+
+def load_books_from_csv(file_path):
+    books = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            book = Book(row['Title'], row['Author'], row['Quantity'])
+            books.append(book)
+    return books
+
+def display_catalog(books):
+    print("Catalog of Available Books:")
+    print("{:<40} {:<30} {:<20}".format("Title", "Author", "Available Quantity"))
+    for book in books:
+        print("{:40} {:<30} {:<20}".format(book.title, book.author, book.available_quantity))
+
+
+def select_books(books):
+    selected_books = []
+    while True:
+        book_title = input("Enter the title of the book you want to checkout (or type 'done' to finish): ")
+        if book_title.lower() == 'done':
+            break
+        found_book = next((book for book in books if book.title.lower() == book_title.lower() and int(book.available_quantity) > 0), None)
+        if found_book:
+            quantity = input(f"How many copies of '{found_book.title}' do you want to checkout? Enter quantity: ")
+            if quantity.isdigit() and int(quantity) > 0:
+                quantity = int(quantity)
+                if quantity <= int(found_book.available_quantity):
+                    selected_books.append((found_book, quantity))
+                    found_book.available_quantity = int(found_book.available_quantity) - quantity
+                else:
+                    print(f"Sorry, only {found_book.available_quantity} copies of '{found_book.title}' available.")
+            else:
+                print("Invalid quantity. Please enter a positive integer greater than zero.")
+        else:
+            print(f"'{book_title}' is either unavailable or not in the catalog.")
+
+    if len(selected_books) > 10:
+        print("Maximum limit of 10 books exceeded.")
+        return -1
+    return selected_books
+
+def calculate_due_date(current_date):
+    return current_date + timedelta(days=14)
+
+def calculate_late_fee(due_date):
+    today = datetime.now()
+    if today > due_date:
+        return (today - due_date).days * 1
+    return 0
+
+def checkout(selected_books):
+    if selected_books == -1:
+        return -1
+
+    print("\nSelected Books for Checkout:")
+    print("Title\t\tQuantity\tDue Date\tLate Fee")
+    for book, quantity in selected_books:
+        due_date = calculate_due_date(datetime.now())
+        late_fee = calculate_late_fee(due_date)
+        print(f"{book.title}\t{quantity}\t\t{due_date.strftime('%Y-%m-%d')}\t{late_fee}")
+
+def return_books(books):
+    returned_books = []
+    while True:
+        book_title = input("Enter the title of the book you are returning (or type 'done' to finish): ")
+        if book_title.lower() == 'done':
+            break
+        found_book = next((book for book in books if book.title.lower() == book_title.lower()), None)
+        if found_book:
+            quantity = input(f"How many copies of '{found_book.title}' are you returning? Enter quantity: ")
+            if quantity.isdigit() and int(quantity) > 0:
+                quantity = int(quantity)
+                found_book.available_quantity += quantity
+                returned_books.append((found_book, quantity))
+            else:
+                print("Invalid quantity. Please enter a positive integer greater than zero.")
+        else:
+            print(f"'{book_title}' is not a valid book to return.")
+
+    total_late_fees = sum(calculate_late_fee(calculate_due_date(datetime.now())) for book, _ in returned_books)
+    print(f"\nTotal late fees for returned books: ${total_late_fees}")
+
+def main():
+    books = load_books_from_csv('books.csv')
+    display_catalog(books)
+
+    action = input("What would you like to do? (checkout/return): ")
+    if action.lower() == 'checkout':
+        selected_books = select_books(books)
+        checkout(selected_books)
+    elif action.lower() == 'return':
+        return_books(books)
+    else:
+        print("Invalid action. Please choose either 'checkout' or 'return'.")
+
+if __name__ == "__main__":
+    main()
---
+++ test_books.py
@@ -0,0 +1,46 @@
+import pytest
+from datetime import datetime, timedelta
+from __main__ import (
+    Book,
+    load_books_from_csv,
+    select_books,
+    calculate_due_date,
+    calculate_late_fee
+)
+# Mock data for testing
+mock_books = [
+    Book("Book1", "Author1", 5),
+    Book("Book2", "Author2", 10),
+    Book("Book3", "Author3", 0),
+    Book("Book4", "Author4", 3)
+]
+
+def test_load_books_from_csv():
+    file_path = 'books_test.csv'
+    books = load_books_from_csv(file_path)
+    assert len(books) > 0  # Assuming the test CSV file contains at least one book entry
+
+def test_select_books():
+    selected_books = select_books(mock_books)
+    assert selected_books != -1  # Asserts that selected_books is not -1, indicating a successful selection
+
+def test_select_books_max_limit():
+    too_many_books = [
+        Book(f"Book{i}", f"Author{i}", 1) for i in range(15)
+    ]
+    selected_books = select_books(too_many_books)
+    assert selected_books == -1  # Asserts that -1 is returned for exceeding the maximum limit
+
+def test_calculate_due_date():
+    current_date = datetime.now()
+    due_date = calculate_due_date(current_date)
+    assert due_date == current_date + timedelta(days=14)  # Asserts due date calculation
+
+def test_calculate_late_fee():
+    current_date = datetime.now()
+    past_date = current_date - timedelta(days=5)
+    late_fee = calculate_late_fee(past_date)
+    assert late_fee == 5  # Assuming $1 per day for 5 days, late fee should be 5
+
+if __name__ == '__main__':
+    pytest.main()
